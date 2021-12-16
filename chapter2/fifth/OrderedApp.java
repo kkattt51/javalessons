c7f49459cae2c6fba70f6160c9b227681baa5957
kkattt51 <10tany2010.10@gmail.com>
2021-12-17 01:00:34 +0300

    created merge() method

--- chapter2/fifth/OrderedApp.java
+++ chapter2/fifth/OrderedApp.java
@@ -1,4 +1,4 @@
-package chapter2.fourth;
+package chapter2.fifth;
 
 class OrdArray {
 	private long[] a;
@@ -31,17 +31,6 @@ class OrdArray {
 			}
 		}
 	}
-	
-	/*public void insert(long value) {
-		int j;
-		for (j = 0; j < nElems; j++) 
-			if (a[j] > value) 
-				break;
-		for (int k = nElems; k > j; k--) 
-			a[k] = a[k - 1];
-		a[j] = value;
-		nElems++;
-	}*/
 	
 	public void insert(long value) {
 		int lowerBound = 0;
@@ -80,42 +69,90 @@ class OrdArray {
 		System.out.println("");
 	}
 	
+	public void merge(OrdArray a1, OrdArray a2) {
+	
+		long[] a3 = new long[a1.nElems + a2.nElems];
+		int count = 0;
+		int i = 0;
+		int j = 0;
+	    for(i = 0; i < a1.nElems; i++) {
+	    	a3[i] = a1.a[i];
+	        count++;
+	    }
+	    for(j = 0; j < a2.nElems; j++) {
+	    	a3[count++] = a2.a[j];
+	    } 
+	    
+	    System.out.println(a3.length);
+	    
+	    for (int out = 0; out < a3.length; out++) {
+	    	for (int in = 1; in < (a3.length - out); in++) {
+	    		if (a3[in - 1] > a3[in]) {
+	    			long temp = a3[in - 1];
+	    			a3[in - 1] = a3[in];
+	    			a3[in] = temp;
+	    		}
+	        }
+	    }
+	    for(int k = 0; k < a3.length; k++) {
+	    	System.out.print(a3[k] + " ");
+	    }
+	}
 }
 
 public class OrderedApp {
 
 	public static void main(String[] args) {
 		int maxSize = 100;
-		OrdArray arr;
-		arr = new OrdArray(maxSize);
+		OrdArray arr1;
+		arr1 = new OrdArray(maxSize);
 		
-		arr.insert(77);
-		arr.insert(99);
-		arr.insert(44);
-		arr.insert(55);
-		arr.insert(22);
-		arr.insert(88);
-		arr.insert(11);
-		arr.insert(00);
-		arr.insert(66);
-		arr.insert(33);
+		arr1.insert(77);
+		arr1.insert(99);
+		arr1.insert(44);
+		arr1.insert(55);
+		arr1.insert(22);
+		arr1.insert(88);
+		arr1.insert(11);
+		arr1.insert(00);
+		arr1.insert(66);
+		arr1.insert(33);
+		
+		OrdArray arr2;
+		arr2 = new OrdArray(maxSize);
+		
+		arr2.insert(10);
+		arr2.insert(12);
+		arr2.insert(36);
+		arr2.insert(75);
+		arr2.insert(54);
+		arr2.insert(28);
+		arr2.insert(91);
+		arr2.insert(69);
+		
+		
 		
 		int searchKey = 55;
-		if (arr.find(searchKey) != arr.size()) 
+		if (arr1.find(searchKey) != arr1.size()) 
 			System.out.println("Found " + searchKey);
 		else 
 			System.out.println("Cannot find " + searchKey);
 		
-		arr.display();
+		arr1.display();
+		arr2.display();
 		
-		arr.delete(00);
-		arr.delete(55);
-		arr.delete(99);
+		OrdArray arr3 = new OrdArray(maxSize);
+		arr3.merge(arr1, arr2);
+		arr3.display();
 		
-		arr.display();
 		
-		arr.insert(69);
-		arr.display();
+		arr1.delete(00);
+		arr1.delete(55);
+		arr1.delete(99);
+		
+		arr1.display();
+		
+		arr1.insert(69);
+		arr1.display();
 	}
-
 }
