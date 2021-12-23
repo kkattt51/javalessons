30c3993fefb8a318ebda94e54b672e5a0802f39b
kkattt51 <10tany2010.10@gmail.com>
2021-12-23 19:27:05 +0300

    created oddEvensort() method

--- chapter3/fourth/BubbleSortApp.java
+++ chapter3/fourth/BubbleSortApp.java
@@ -20,25 +20,70 @@ class ArrayBub {
 		System.out.println("");
 	}
 	
-	public void bubbleSort() {
-		int out, in, left;
-		
-		for (out = nElems - 1; out > 1; out--) 
-			for (in = 0; in < out; in++) 
-				if (a[in] > a[in + 1]) 
-					swap(in, in + 1);	
-				
-		for (left = 0; left < nElems - 1; left++) 
-			//for (in = 0; in < nElems - left - 1; in++)
-			for(in = nElems - 1; in > left - 1; in--)
-				if (a[in] < a[in + 1]) 
-					swap(in + 1, in);
+	public void oddEvenSort() {
+	    for (int i = 0; i < nElems; i++) {
+	        if (i % 2 == 0) {
+	        // нечетная итерация
+	            for (int j = 0; j <= nElems - 2; j = j + 2) { 
+	                // условие j <= nElems - 2 нужно чтобы a[j+1] не выходило за 
+	                // пределы массива (не больше nElems-1)
+	                // j = j + 2 для перехода к следующей паре для сравнения
+	                swap(j, j + 1);
+	                // если левый элемент больше, меняем местами
+	            }
+	        }
+	        else 
+	        // четная итерация
+	        {       
+	            for (int j = 1; j <= nElems - 2; j = j + 2) {
+	                swap(j, j + 1);
+	            }
+	        }
+	    }
 	}
 	
+//	public void bubbleSort() {
+//		int right = nElems - 1;
+//		int left = 0;
+//		
+//		do {
+//			for (int in = left; in < right; in++) {	
+//				if (a[in] > a[in + 1]) {
+//					swap(in, in + 1);
+//				}
+//			}
+//			right--;
+//			for (int in = right; in > left; in--) {
+//				if (a[in] < a[in - 1]) {
+//					swap(in, in - 1);
+//				}
+//			}
+//			left++;
+//		} while (right > left);
+//	}
+	
+//	public void bubbleSort() {
+//		int out, in, left;
+//		
+//		for (out = nElems - 1; out > 1; out--) 
+//			for (in = 0; in < out; in++) 
+//				if (a[in] > a[in + 1]) 
+//					swap(in, in + 1);	
+//				
+//		for (left = 0; left < nElems - 1; left++) 
+//			//for (in = 0; in < nElems - left - 1; in++)
+//			for(in = nElems - 1; in > left - 1; in--)
+//				if (a[in] < a[in + 1]) 
+//					swap(in + 1, in);
+//	}
+	
 	private void swap(int one, int two) {
-		long temp = a[one];
-		a[one] = a[two];
-		a[two] = temp;
+		if(a[one] > a[two])
+	    {
+	        long temp = a[one];
+	        a[one] = a[two];
+	        a[two] = temp;
+	    }
 	}
 }
 
@@ -61,8 +106,8 @@ public class BubbleSortApp {
 		arr.insert(33);
 		
 		arr.display();
-		
-		arr.bubbleSort();
+		arr.oddEvenSort();
+		//arr.bubbleSort();
 		arr.display();
 	}
 }
