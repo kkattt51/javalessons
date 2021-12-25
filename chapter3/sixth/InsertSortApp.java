6fa106450d3e8c2caeb70801712ec6099375affa
kkattt51 <10tany2010.10@gmail.com>
2021-12-25 22:00:15 +0300

    updated insertion() method

---
+++ chapter3/sixth/InsertSortApp.java
@@ -0,0 +1,110 @@
+package chapter3.sixth;
+
+
+class ArrayIns {
+	private long[] a;
+	private int nElems;
+	
+	public ArrayIns(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+	
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems++;
+	}
+	
+	public void display() {
+		for (int j = 0; j < nElems; j++) 
+			System.out.print(a[j] + " ");
+		System.out.println("");
+	}
+	
+	public void insertionSort() {
+		int in, out;
+		
+		for (out = 1; out < nElems; out++) {
+			long temp = a[out];
+			in = out;
+			while (in > 0 && temp <= a[in - 1]) {
+			    if(temp == a[in - 1]) temp = -1;
+			    a[in] = a[in - 1];
+
+			    in--;
+			}
+			a[in] = temp;
+			
+			
+		}
+	}
+	
+	public void noDups() {
+		System.out.println("Array without duplicates: ");
+		int j = 0;
+		
+	    for (int i = 0; i < nElems - 1; i++) {
+	        if (a[i] != a[i + 1]) {
+	            a[j] = a[i];
+	            j++;
+	        }  
+	    }
+	    a[j++] = a[nElems - 1];
+	    
+	    for (int i = 0; i < j; i++) {
+	        System.out.print(a[i] + " "); 
+	    }
+	    System.out.println(""); 
+	  }
+	
+	public void median() {
+		//insertionSort();
+		
+		double med;
+		
+		if (nElems % 2 == 0) {
+		    med = ((double)a[nElems / 2] + (double)a[nElems/2 - 1]) / 2;
+		    System.out.println("Median = " + med);
+		} else {
+			med = (double)a[nElems / 2];
+			System.out.println("Median = " + med);
+		}
+		  
+	}
+}
+
+public class InsertSortApp {
+
+	public static void main(String[] args) {
+		int maxSize = 100;
+		ArrayIns arr;
+		arr = new ArrayIns(maxSize);
+		
+		arr.insert(77);
+		arr.insert(99);
+		arr.insert(44);
+		arr.insert(55);
+		arr.insert(17);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(17);
+		arr.insert(11);
+		arr.insert(11);
+		arr.insert(00);
+		arr.insert(66);
+		arr.insert(17);
+		arr.insert(33);
+		
+		arr.display();
+		
+		System.out.println("Sorted array: ");
+		arr.insertionSort();
+		arr.display();
+		
+		arr.median();
+		
+		//arr.noDups();
+	}
+
+}
+
